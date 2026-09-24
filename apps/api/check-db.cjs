1897c839c1e9e6a611f70b75774f4c34014bc217
Devran Perdana Malik <devranmalik82@gmail.com>
2026-09-24 08:49:16 +0700

    chore: remove deprecated database check scripts and update DATABASE_URL in environment configuration

--- apps/api/check-db.cjs
+++
@@ -1,5 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
-prisma.appModule.findMany()
-  .then(modules => console.log(modules.map(m => ({id: m.id, callback: m.callbackUrl}))))
-  .finally(() => prisma.$disconnect());
--- apps/api/gen-pw.cjs
+++
@@ -1,6 +0,0 @@
-const { hash } = require('@node-rs/argon2');
-async function generate() {
-  const h = await hash('TzWdn_2106');
-  console.log(h);
-}
-generate();
